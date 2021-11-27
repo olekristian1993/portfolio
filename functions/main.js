a08ba6b0367752ebe7b57bdd986c0b86f33c7690
olekristian1993 <69903623+olekristian1993@users.noreply.github.com>
2021-11-27 02:33:20 +0100

    Scrolljump
    La til scrolljump med knapper

--- functions/main.js
+++ functions/main.js
@@ -5,3 +5,19 @@ $(document).ready(function () {
     return $(this).attr("aria-valuenow") + "%";
   });
 });
+
+function jumpScroll1() {
+  window.scroll(0, 0);
+}
+
+function jumpScroll2() {
+  window.scroll(0, 920);
+}
+
+function jumpScroll3() {
+  window.scroll(0, 920 * 2);
+}
+
+function jumpScroll4() {
+  window.scroll(0, 920 * 3);
+}
